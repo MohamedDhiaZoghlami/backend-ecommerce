59d11a7584d10e3b141f56de756029eab2fcd707
MohamedDhiaZoghlami <mohameddhia.zoghlami@esprit.tn>
2022-08-13 16:10:53 +0100

    stripe integration

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
+//routes
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productsRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 dotenv.config();
 
@@ -19,12 +22,14 @@ mongoose
     console.log("error connecting to db!!!");
   });
 
+app.use(cors);
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("backend is running ON :", process.env.PORT);
